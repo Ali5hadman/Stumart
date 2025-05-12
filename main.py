df526e87acb86e371696c45bb567c4da9a989a2d
AlishadmanORB <alishadman@hotmail.com>
2025-05-12 10:55:33 +0200

    Final commit

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ import gradio as gr
 from model import gemma_response
 from fetch_email import fetch_email
 
+import requests
+from bs4 import BeautifulSoup
+
+import requests
+from bs4 import BeautifulSoup
+import urllib.parse
+
+def bing_search(query, max_results=10):
+    params = {"q": query, "count": max_results}
+    resp = requests.get("https://www.bing.com/search", params=params)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    links = []
+    for h2 in soup.select("li.b_algo h2"):
+        a = h2.find("a")
+        if a and a["href"]:
+            title = a.get_text()
+            url = a["href"]
+            links.append((title, url))
+    return links
+
+
 
 with open("instructions.txt", "r", encoding="utf-8") as file:
     instructions_str = file.read()
@@ -47,6 +68,17 @@ with gr.Blocks() as demo:
             email_response = fetch_email(5) 
             email_response = "Give me a detailed Summarize this emails for me and print it: " + email_response
             bot_response,_ = gemma_response(email_response, instruction = instructions_str, history=formatted_history)
+        
+        if 'SEARCH' in bot_response:
+            search_query = bot_response.split(', ')[1].strip()
+            if '?' in search_query:
+                search_query = search_query.split('?')[0].strip()
+
+            print("search_query", search_query)
+            search_results = str(bing_search(search_query))
+            print("search_results", search_results)
+            sumarized_requested = "This is a result of a search result regarding " + search_query+ ". tell me what you find in these results in few sentences and give a complete responce: " + search_results
+            bot_response, _ = gemma_response(sumarized_requested, instruction = instructions_str, history=formatted_history)
         
         
         # Update internal history for the model
